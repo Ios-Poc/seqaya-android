ec2de0608aad5054e7ca42b9cd1c14030fecc797
Ahmed Mesawer <AhmedMesawer@github.com>
2018-03-26 13:14:19 +0200

    registration fragment

---
+++ app/src/main/java/com/ntg/user/sa2aia/CompanyRegistrationFragment.java
@@ -0,0 +1,28 @@
+package com.ntg.user.sa2aia;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CompanyRegistrationFragment extends Fragment {
+
+
+    public CompanyRegistrationFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_company_registeration, container, false);
+    }
+
+}
