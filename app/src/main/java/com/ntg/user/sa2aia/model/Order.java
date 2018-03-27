64a95c31ba0b261cbfc09df77eaa49537b935ad7
Sara Elmoghazy <sara.elmoghazy@yahoo.com>
2018-03-27 13:48:58 +0200

    add shopping card

--- app/src/main/java/com/ntg/user/sa2aia/model/Order.java
+++ app/src/main/java/com/ntg/user/sa2aia/model/Order.java
@@ -1,7 +1,6 @@
 package com.ntg.user.sa2aia.model;
 
 import java.io.Serializable;
-import java.util.List;
 
 /**
  * Created by Sara Elmoghazy on 26/03/2018.
@@ -9,21 +8,14 @@ import java.util.List;
 
 public class Order implements Serializable {
 
-    private List<OrderItem> orderItemList;
+    private ShoppingCartClient shoppingCartClient;
     private String date;
     private String location;
     private @PaymentMethod
     String paymentMethod;
     private String time;
+    private String userName;
 
-
-    public List<OrderItem> getOrderItemList() {
-        return orderItemList;
-    }
-
-    public void setOrderItemList(List<OrderItem> orderItemList) {
-        this.orderItemList = orderItemList;
-    }
 
     public String getDate() {
         return date;
@@ -55,5 +47,21 @@ public class Order implements Serializable {
 
     public void setTime(String time) {
         this.time = time;
+    }
+
+    public ShoppingCartClient getShoppingCartClient() {
+        return shoppingCartClient;
+    }
+
+    public void setShoppingCartClient(ShoppingCartClient shoppingCartClient) {
+        this.shoppingCartClient = shoppingCartClient;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 }
