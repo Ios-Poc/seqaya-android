d44b936254f091c8ff06a693a1b0d1d475293604
Ahmed Mesawer <AhmedMesawer@github.com>
2018-03-27 13:26:07 +0200

    login

---
+++ app/src/main/java/com/ntg/user/sa2aia/User.java
@@ -0,0 +1,14 @@
+package com.ntg.user.sa2aia;
+
+/**
+ * Created by ilias on 27/03/2018.
+ */
+
+public class User {
+
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNumber;
+}
